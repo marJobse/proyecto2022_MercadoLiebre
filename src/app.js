9048016238aa9f5b92bf6e283222213125a09753
mariana.jobse <jobsemarr@yahoo.com.ar>
2022-07-16 13:42:15 -0300

    rutas originales

--- src/app.js
+++ src/app.js
@@ -7,15 +7,27 @@ app.use(express.static(publicPath));
 app.listen(process.env.PORT || 3000, () => console.log('Servidor corriendo en el puerto 3000'));
 
 // Ruta del Home
-const mainRoutes = require('./routes/mainRoutes');
-app.use("/", mainRoutes)
+//const mainRoutes = require('./routes/mainRoutes');
+//app.use("/", mainRoutes)
 
 // Ruta del Register y Login (mismo controlador)
 
-const usersRoutes = require('./routes/usersRoutes');
-app.use("/users", usersRoutes)
+//const usersRoutes = require('./routes/usersRoutes');
+//app.use("/users", usersRoutes)
 
 
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'));
+
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+
+});
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'));
+});
 
 
