c425e847f37261277539054953f8e035f7526d58
mariana.jobse <jobsemarr@yahoo.com.ar>
2022-07-16 12:05:15 -0300

    rutas

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 //const publicPath = path.resolve(__dirname, './public');
 const publicPath = path.join(__dirname, './public');
-
+const homeRouter = require('./routes/homeRouter');
 
 app.use(express.static(publicPath));
 
@@ -11,6 +11,7 @@ app.use(express.static(publicPath));
 app.listen(process.env.PORT || 3000, () => console.log('Servidor corriendo en el puerto 3000'));
 
 
+app.use("/", homeRouter)
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, './views/home.html'));
